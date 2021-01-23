a2fa24feb575edc22d2db1693d4388132113949a
Piotr Gorski <gorskipiotr5@gmail.com>
2021-01-23 21:09:13 +0000

    my first if statement

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let a = 12;
+
+if (a > 10) {
+    console.log('Podana liczba jest wieksza od 10');
+} else {
+    console.log("Podana liczba nie jest wieksza od 10");
+}
+
+/* wersja bez srednikow:
+
+let a = 12
+
+if (a > 10) {
+    console.log('Podana liczba jest wieksza od 10')
+} else {
+    console.log("Podana liczba nie jest wieksza od 10")
+} */
+
+
